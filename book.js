7a75c79f9559ec5e08c74dd887e569f13eae2605
Aseem <aseem.tim@gmail.com>
2020-12-29 21:43:58 +0545

    Added delete function

--- book.js
+++ book.js
@@ -14,14 +14,15 @@ function addBookToLibrary(){
     inputRead=prompt("Enter the read status of the book","Read/Not read yet");
     let myBook = new Book(inputTitle,inputAuthor,inputPages,inputRead);
     myLibrary.push(myBook);
-    displayBook();
+    createBook();
     console.log(myLibrary);
 }
 
-function displayBook(){
-
-    for(let i=myLibrary.length-1;i<myLibrary.length;i++){
+function createBook(){
+    clearScreen();
+    for(let i=0;i<myLibrary.length;i++){
         let bookTable = document.createElement('table');
+        bookTable.setAttribute('class','bookTable')
         for (const key in myLibrary[i]) {
             console.log(key);
             let tableRow = document.createElement('tr');
@@ -34,20 +35,34 @@ function displayBook(){
             tableRow = document.createElement('tr');
             rowData = document.createElement('td');
             if(j==0){
-                rowData.innerHTML="<button class='changeStatus'>Change Status</button";
+                rowData.innerHTML="<button onclick = '' class='changeStatus'>Change Status</button";
             }
             else{
-                rowData.innerHTML="<button class='deleteBook'>Delete Book</button>"
+                rowData.innerHTML="<button class='deleteBook' onclick = 'delBook(this.parentNode.dataset.index)'>Delete Book</button>";
+                rowData.setAttribute('data-index', i);
             }
             tableRow.appendChild(rowData);
             bookTable.appendChild(tableRow);
         };
         bookContainer.appendChild(bookTable);
+        console.log(rowData);
+        console.log(bookContainer.childElementCount );
     }
 }
 
-const bookContainer = document.querySelector(".displayBooks");
+function clearScreen(){
+    while(document.querySelector('.bookTable')){
+        bookContainer.removeChild(document.querySelector('.bookTable'));
+    }
+}
 
+function delBook(indexNum) {
+    myLibrary.splice(indexNum,1);
+    console.log(myLibrary);
+    createBook();
+}
+
+const bookContainer = document.querySelector(".displayBooks");
 let inputTitle;
 let inputAuthor;
 let inputPages;
@@ -56,3 +71,5 @@ let inputRead;
 
 const addBtn = document.querySelector('#newBook');
 addBtn.onclick= () => addBookToLibrary();
+
+
